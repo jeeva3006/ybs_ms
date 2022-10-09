9efad526dda2bd377050838384860701edb5d1f2
jeeva3006 <jeevasubash64@gmail.com>
2022-10-09 16:28:52 +0530

    Changed trigger time.

--- src/schedule.js
+++ src/schedule.js
@@ -9,7 +9,7 @@ class ybs extends youtube {
         super();
         this.scheduleTime = 1;
         this.mongo = new Mongo;
-        this.cronRule = `00 00 18 * * *`;
+        this.cronRule = `00 00 17 * * *`;
     }
 
     async schedule() {
