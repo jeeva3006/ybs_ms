2164222f2c412c8ebf3db9b81913cbf69dd29c62
jeeva3006 <jeevasubash64@gmail.com>
2022-10-09 16:37:02 +0530

    changed time

--- src/schedule.js
+++ src/schedule.js
@@ -9,7 +9,7 @@ class ybs extends youtube {
         super();
         this.scheduleTime = 1;
         this.mongo = new Mongo;
-        this.cronRule = `00 06 11 * * *`;
+        this.cronRule = `00 08 11 * * *`;
     }
 
     async schedule() {
