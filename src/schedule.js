795935d986a9c87395a0b1395c617353e904a887
jeeva3006 <jeevasubash64@gmail.com>
2022-10-09 19:07:59 +0530

    trigger time.

--- src/schedule.js
+++ src/schedule.js
@@ -9,7 +9,7 @@ class ybs extends youtube {
         super();
         this.scheduleTime = 1;
         this.mongo = new Mongo;
-        this.cronRule = `00 05 * * * *`;
+        this.cronRule = `00 40 * * * *`;
     }
 
     async schedule() {
