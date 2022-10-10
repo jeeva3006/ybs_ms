b35a0855f73a8354419113920714825066f6c352
jeeva3006 <jeevasubash64@gmail.com>
2022-10-10 21:22:01 +0530

    express added.

--- src/schedule.js
+++ src/schedule.js
@@ -9,7 +9,7 @@ class ybs extends youtube {
         super();
         this.scheduleTime = 1;
         this.mongo = new Mongo;
-        this.cronRule = `00 50 * * * *`;
+        this.cronRule = `00 25 * * * *`;
     }
 
     async schedule() {
