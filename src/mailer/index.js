964c4dc8d2530fbd0694634c0bf31893dc10c723
jeeva3006 <jeevasubash64@gmail.com>
2022-10-13 21:39:43 +0530

    Email only on trending video.

--- src/mailer/index.js
+++ src/mailer/index.js
@@ -17,37 +17,39 @@ const readHTMLFile = function (path, callback) {
 };
 
 function Mailer(videos) {
-    const date = moment().format('DD MMM YYYY hh:mm');
     const data = { date, videos };
+    const date = moment().format('DD MMM YYYY hh:mm');
 
-    readHTMLFile(htmlPath, function (err, html) {
-        if (!err) {
-            const template = handlebars.compile(html);
-            let htmlToSend = videos && videos.length > 0 ? template(data) : `<h3> No videos found</h3>`;
+    if (videos && videos.length > 0) {
+        readHTMLFile(htmlPath, function (err, html) {
+            if (!err) {
+                const template = handlebars.compile(html);
+                let htmlToSend = videos && videos.length > 0 ? template(data) : `<h3> No videos found</h3>`;
 
-            const transporter = nodemailer.createTransport({
-                service: "gmail",
-                secure: true,
-                port: 587,
-                auth: {
-                    user: 'styls360@gmail.com',
-                    pass: appPassWord,
-                },
-            });
-
-            transporter.sendMail({
-                from: "styls360@gmail.com",
-                to: "jeevasubash64@gmail.com",
-                subject: `${videos.length} video(s) on ${date}`,
-                text: "Hello world?",
-                html: htmlToSend
-            },
-                function (error) {
-                    if (error) console.log(error);
-                    else console.log(`Email sent for ${date}`);
+                const transporter = nodemailer.createTransport({
+                    service: "gmail",
+                    secure: true,
+                    port: 587,
+                    auth: {
+                        user: 'styls360@gmail.com',
+                        pass: appPassWord,
+                    },
                 });
-        }
-    });
+
+                transporter.sendMail({
+                    from: "styls360@gmail.com",
+                    to: "jeevasubash64@gmail.com",
+                    subject: `${videos.length} video(s) on ${date}`,
+                    text: "Hello world?",
+                    html: htmlToSend
+                },
+                    function (error) {
+                        if (error) console.log(error);
+                        else console.log(`Email sent for ${date}`);
+                    });
+            }
+        });
+    }
 }
 
 module.exports = Mailer;
