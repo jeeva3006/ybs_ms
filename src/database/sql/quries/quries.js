01cbaa756c16beeb5b559db3a670a14c8576791a
jeeva3006 <jeevasubash64@gmail.com>
2022-11-07 21:40:59 +0530

    order by desc.

--- src/database/sql/quries/quries.js
+++ src/database/sql/quries/quries.js
@@ -1,7 +1,7 @@
 const insertVideo = `INSERT INTO videos VALUES (?);`;
 const getVideoIds = `select videoId from videos`;
 const insertDescription = `INSERT INTO description VALUES (?);`;
-const getVideosToPost = `select v.*, d.description, d.tags from videos v, description d where v.id = d.id and v.isPublished is false order by v.publishedAt;`;
+const getVideosToPost = `select v.*, d.description, d.tags from videos v, description d where v.id = d.id and v.isPublished is false order by v.publishedAt desc;`;
 const setPostDetails = `insert into postDetails values (?);`;
 const setIsPublished = `update videos set isPublished = true where id = ?;`;
 const getPublishedCount = `select videoId from videos where isPublished = true;`;
